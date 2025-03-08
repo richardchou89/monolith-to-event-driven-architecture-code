cfa184818045b44aa7d4f2426180b07bef61bb08
Richard Chou <konami99@hotmail.com>
2025-03-08 21:50:23 +1100

    updated handler and sam template

--- microservices/SendEmailService/hello-world/main.go
+++ microservices/SendEmailService/hello-world/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	_ "github.com/lib/pq"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var greeting string
-	sourceIP := request.RequestContext.Identity.SourceIP
+func handler(ctx context.Context, event events.SQSEvent) error {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbPass := os.Getenv("DB_PASS")
+	dbSSLMode := os.Getenv("DB_SSL_MODE")
 
-	if sourceIP == "" {
-		greeting = "Hello, world!\n"
-	} else {
-		greeting = fmt.Sprintf("Hello, %s!\n", sourceIP)
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", dbHost, dbPort, dbUser, dbPass, dbName, dbSSLMode)
+
+	sql.Open("postgres", connStr)
+
+	//send email
+	for _, record := range event.Records {
+		if *record.MessageAttributes["eventType"].StringValue == "send_email" {
+			//send email
+		} else if *record.MessageAttributes["eventType"].StringValue == "archive_email" {
+			//archive email
+		}
 	}
 
-	return events.APIGatewayProxyResponse{
-		Body:       greeting,
-		StatusCode: 200,
-	}, nil
+	return nil
 }
 
 func main() {
